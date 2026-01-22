331bf07b976e8324c4f4e68f0127d9bcc2ca5afb
even flow <guy99992@gmail.com>
2026-01-21 22:21:15 -0600

    Intial commit

---
+++ src/main/java/frc/robot/Shooter.java
@@ -0,0 +1,95 @@
+package frc.robot;
+
+import com.revrobotics.spark.SparkMax;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Rotations;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.local.SparkWrapper;
+
+public class Shooter extends SubsystemBase {
+    private SparkMax raw_shooter_motor0 = new SparkMax(51, MotorType.kBrushless);
+    // private SparkMax raw_shooter_motor1 = new SparkMax(52, MotorType.kBrushless);
+
+
+    private SmartMotorControllerConfig shooter_motor_config = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withClosedLoopController(1, 0, 0)
+        .withSimClosedLoopController(0.01, 0, 0)
+        .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+        .withSimFeedforward(new SimpleMotorFeedforward(0, 1, 0))
+
+        .withGearing(1)
+        .withStatorCurrentLimit(Amps.of(40))
+        // .withSupplyCurrentLimit(Amps.of(40))
+        .withIdleMode(MotorMode.COAST)
+
+        .withWheelDiameter(Inches.of(3))
+        .withMomentOfInertia(Inches.of(3.95), Pounds.of(1.54))
+
+        .withMotorInverted(false)
+
+        .withTelemetry("MOTORshooter", TelemetryVerbosity.HIGH)
+
+        // .withFollowers(Pair.of(raw_shooter_motor1, true))
+        ;
+
+    private SmartMotorController smc = new SparkWrapper(raw_shooter_motor0, DCMotor.getNEO(1), shooter_motor_config);
+
+    private final FlyWheelConfig shooterConfig = new FlyWheelConfig(smc)
+        // Diameter of the flywheel.
+        .withDiameter(Inches.of(3.95))
+        // Mass of the flywheel.
+        .withMass(Pounds.of(1.54))
+        // Telemetry name and verbosity for the arm.
+        .withTelemetry("MECHshooter", TelemetryVerbosity.HIGH);
+
+    // Shooter Mechanism
+    private FlyWheel shootermech = new FlyWheel(shooterConfig);
+
+
+    public Shooter() {
+
+    }
+
+    public Command setVelocity(AngularVelocity velocity) {
+        return shootermech.setSpeed(velocity);
+    }
+
+    public AngularVelocity getVelocity() {
+        return AngularVelocity.ofBaseUnits(shootermech.getSpeed().in(RPM), RPM);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        SmartDashboard.putNumber("Fcuker2", getVelocity().baseUnitMagnitude());
+        shootermech.updateTelemetry();
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        // This method will be called once per scheduler run during simulation
+        shootermech.simIterate();
+    }
+}
